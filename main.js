52bae7a26a5b6a434f424054aeaa6bdf13198141
RealByter <justavishay@gmail.com>
2023-02-27 14:26:53 +0200

    feat: add function to play one round

--- main.js
+++ main.js
@@ -14,3 +14,40 @@ function getComputerChoice() {
 			return 'scissors';
 	}
 }
+
+function playRound(playerSelection, computerSelection) {
+	let result = '';
+	playerSelection = playerSelection.toLowerCase();
+
+	if (playerSelection == 'rock') {
+		if (computerSelection == 'rock') {
+			result = "Tie! Rock doesn't beat Rock";
+		} else if (computerSelection == 'paper') {
+			result = 'You Lose! Paper beats Rock';
+		} else {
+			result = 'You Win! Rock beats Paper';
+		}
+	} else if (playerSelection == 'paper') {
+		if (computerSelection == 'paper') {
+			result = "Tie! Paper doesn't beat Paper";
+		} else if (computerSelection == 'scissors') {
+			result = 'You Lose! Scissors beats Paper';
+		} else {
+			result = 'You Win! Paper beats Rock';
+		}
+	} else {
+		if (computerSelection == 'scissors') {
+			result = "Tie! Scissors doesn't beat Scissors";
+		} else if (computerSelection == 'rock') {
+			result = 'You Lose! Rock beats Scissors';
+		} else {
+			result = 'You Win! Scissors beats Paper';
+		}
+	}
+
+	return result;
+}
+
+const playerSelection = 'rock';
+const computerSelection = getComputerChoice();
+console.log(playRound(playerSelection, computerSelection));
